1a1ad223b8a8c0e3a235c265285703bbf8518cce
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-11-09 22:53:14 +0300

    update ,NET to 7

--- Src/MathEngineTests/ParserHelperFixture.cs
+++ Src/MathEngineTests/ParserHelperFixture.cs
@@ -10,38 +10,38 @@ namespace MathEngineTests
         [Test]
         public void IsNumber()
         {
-            Assert.That(ParserHelper.IsNumber("0".AsSpan()), Is.EqualTo(1));
-            Assert.That(ParserHelper.IsNumber("0+1".AsSpan()), Is.EqualTo(1));
-            Assert.That(ParserHelper.IsNumber("0 + 1".AsSpan()), Is.EqualTo(1));
-            Assert.That(ParserHelper.IsNumber("0.132544".AsSpan()), Is.EqualTo(8));
-            Assert.That(ParserHelper.IsNumber("456213".AsSpan()), Is.EqualTo(6));
+            Assert.That(ParserHelper.IsNumber("0".AsSpan(), false), Is.EqualTo(1));
+            Assert.That(ParserHelper.IsNumber("0+1".AsSpan(), false), Is.EqualTo(1));
+            Assert.That(ParserHelper.IsNumber("0 + 1".AsSpan(), false), Is.EqualTo(1));
+            Assert.That(ParserHelper.IsNumber("0.132544".AsSpan(), false), Is.EqualTo(8));
+            Assert.That(ParserHelper.IsNumber("456213".AsSpan(), false), Is.EqualTo(6));
             
-            Assert.That(ParserHelper.IsNumber("456.5 2135".AsSpan()), Is.EqualTo(5));
+            Assert.That(ParserHelper.IsNumber("456.5 2135".AsSpan(), false), Is.EqualTo(5));
 
-            Assert.That(ParserHelper.IsNumber(" 456213".AsSpan()), Is.EqualTo(-1));
-            Assert.That(ParserHelper.IsNumber("s".AsSpan()), Is.EqualTo(-1));
-            Assert.That(ParserHelper.IsNumber("!".AsSpan()), Is.EqualTo(-1));
+            Assert.That(ParserHelper.IsNumber(" 456213".AsSpan(), false), Is.EqualTo(-1));
+            Assert.That(ParserHelper.IsNumber("s".AsSpan(), false), Is.EqualTo(-1));
+            Assert.That(ParserHelper.IsNumber("!".AsSpan(), false), Is.EqualTo(-1));
 
             Assert.That(
-                () => ParserHelper.IsNumber("001254.5".AsSpan()),
+                () => ParserHelper.IsNumber("001254.5".AsSpan(), false),
                 Throws.Exception.TypeOf(typeof(ArgumentException)).And
                 .Message.EqualTo("Incorrect number")
                 );
 
             Assert.That(
-                () => ParserHelper.IsNumber("12.54.5".AsSpan()),
+                () => ParserHelper.IsNumber("12.54.5".AsSpan(), false),
                 Throws.Exception.TypeOf(typeof(ArgumentException)).And
                 .Message.EqualTo("Double separator in number '12.54.'")
                 );
 
             Assert.That(
-                () => ParserHelper.IsNumber("12.".AsSpan()),
+                () => ParserHelper.IsNumber("12.".AsSpan(), false),
                 Throws.Exception.TypeOf(typeof(ArgumentException)).And
                 .Message.EqualTo("Unexpected end of number '12.'")
                 );
 
             Assert.That(
-                () => ParserHelper.IsNumber(Span<char>.Empty),
+                () => ParserHelper.IsNumber(Span<char>.Empty, false),
                 Throws.Exception.TypeOf(typeof(ArgumentException)).And
                 .Message.EqualTo("Empty span")
                 );
